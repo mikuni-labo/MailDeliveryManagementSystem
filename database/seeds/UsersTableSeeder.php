d1f71eb9acbd16920dda29d7737e66930b0740e9
Kuniyasu Wada <k-wada@mikunilabo.com>
2017-08-19 17:31:10 +0900

    管理者シード作成

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Schema::disableForeignKeyConstraints();
+
+        DB::table('users')->truncate();
+
+        Schema::enableForeignKeyConstraints();
+
+        DB::table('users')->insert([
+            [
+                'id'           => 1,
+                'name'         => '管理者',
+                'email'        => 'test@admin.jp',
+                'password'     => Hash::make('p1p1p1p1'),
+//                 'status'       => true,
+                'created_at'   => Carbon::now(),
+                'updated_at'   => Carbon::now(),
+//                 'confirmed_at' => Carbon::now(),
+            ],
+        ]);
+    }
+
+}
