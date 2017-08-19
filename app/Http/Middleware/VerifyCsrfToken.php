72f21489777bb490049aa8a06bc52a78bd66caf1
Kuniyasu Wada <k-wada@mikunilabo.com>
2017-08-19 21:48:14 +0900

    CSRFトークンミドルウェアにてHTTP408を吐くように調整

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class VerifyCsrfToken extends BaseVerifier
 {
@@ -14,4 +16,30 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         //
     ];
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     *
+     * @throws \Illuminate\Session\TokenMismatchException
+     *
+     * {@inheritDoc}
+     * @see \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::handle()
+     */
+    public function handle($request, Closure $next)
+    {
+        if (
+            $this->isReading($request) ||
+            $this->runningUnitTests() ||
+            $this->inExceptArray($request) ||
+            $this->tokensMatch($request)
+            ) {
+                return $this->addCookieToResponse($request, $next($request));
+        }
+
+        throw new HttpException(408);
+    }
 }
