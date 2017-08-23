6677f1d5a09e5a499a91d87ae7e7562b9b6a83a8
Kuniyasu Wada <k-wada@mikunilabo.com>
2017-08-23 17:24:02 +0900

    メールテンプレートテーブルマイグレーションとモデル作成

---
+++ app/Models/MailTemplate.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class MailTemplate extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'subject',
+        'content',
+        'from',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        //
+    ];
+
+    /**
+     * The attributes for cast to Carbon.
+     *
+     * @var array
+     */
+    protected $dates = [
+        //
+    ];
+
+    /**
+     * The attributes for casts.
+     *
+     * @var array
+     */
+    protected $casts = [
+        //
+    ];
+
+    protected $perPage = 20;
+
+}
