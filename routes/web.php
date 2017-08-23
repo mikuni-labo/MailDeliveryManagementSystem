bf13b2b70e69378f4d11777927fca27289da3044
Kuniyasu Wada <k-wada@mikunilabo.com>
2017-08-23 22:05:14 +0900

    ルーティングをグルーピング

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,48 @@
 |
 */
 
-Route::get( '/',            'HomeController@index')->name('home');
+Route::group([
+    'middleware' => [
+        'web',
+    ],
+], function() {
+    Route::get( '/', 'HomeController@index')->name('home');
 
-Auth::routes();
+    /**
+     * 認証
+     */
+    Auth::routes();
 
-Route::get( '/modify',      'Auth\ModifyController@index')->name('modify');
-Route::post('/modify',      'Auth\ModifyController@update');
+    Route::group([
+        'prefix'     => '',
+        'namespace'  => 'Auth',
+    ], function() {
+        Route::get( 'modify',   'ModifyController@index')->name('modify');
+        Route::post('modify',   'ModifyController@update');
+    });
+
+    /**
+     * 来場者管理
+     */
+//     Route::group([
+//         'prefix'     => 'visitor',
+//         'namespace'  => 'Visitor',
+//     ], function() {
+//
+//     });
+
+    /**
+     * メール配信管理
+     */
+    Route::group([
+        'prefix'     => 'mail',
+        'namespace'  => 'Mail',
+    ], function() {
+        Route::get( 'add',    'StoreController@index')->name('mail.add');
+        Route::post('add',    'StoreController@store');
+
+        Route::get( 'edit',   'ModifyController@index')->name('mail.edit');
+        Route::post('edit',   'ModifyController@update');
+    });
+});
+
