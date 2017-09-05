21e03647966b2bf45b8061f6943a9020dfc15754
Kuniyasu Wada <k-wada@mikunilabo.com>
2017-09-05 23:51:13 +0900

    配信セットシーダー作成

---
+++ database/seeds/DeliverySetsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DeliverySetsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Schema::disableForeignKeyConstraints();
+
+        DB::table('delivery_sets')->truncate();
+
+        Schema::enableForeignKeyConstraints();
+
+        DB::table('delivery_sets')->insert([
+            [
+                'id'               => 1,
+                'mail_template_id' => 1,
+                'name'             => 'セット1',
+                'data'             => '[]',
+                'status'           => true,
+                'created_at'   => Carbon::now(),
+                'updated_at'   => Carbon::now(),
+//                 'deleted_at'   => Carbon::now(),
+            ],
+        ]);
+    }
+
+}
