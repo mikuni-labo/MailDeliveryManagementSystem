2d9c7fee765b72e504d127a6842365085f3c0e11
Kuniyasu Wada <k-wada@mikunilabo.com>
2017-09-06 16:20:37 +0900

    共通フォームをメソッド毎に利用出来るように調整

--- resources/views/common/form/common.blade.php
+++ resources/views/common/form/common.blade.php
@@ -2,10 +2,22 @@
 {!! Form::open(['id' => 'logout-form', 'url' => route('logout'), 'method' => 'post', 'style' => 'display: none;']) !!}
 {!! Form::close() !!}
 
-<!-- Delete -->
+<!-- GET -->
+{!! form::open(['id' => 'get-form', 'url' => '', 'method' => 'get', 'style' => 'display: none;']) !!}
+{!! form::close() !!}
+
+<!-- POST -->
+{!! form::open(['id' => 'post-form', 'url' => '', 'method' => 'post', 'style' => 'display: none;']) !!}
+{!! form::close() !!}
+
+<!-- PUT -->
+{!! Form::open(['id' => 'put-form', 'url' => '', 'method' => 'put', 'style' => 'display: none;']) !!}
+{!! Form::close() !!}
+
+<!-- DELETE -->
 {!! form::open(['id' => 'delete-form', 'url' => '', 'method' => 'delete', 'style' => 'display: none;']) !!}
 {!! form::close() !!}
 
-<!-- Restore -->
-{!! Form::open(['id' => 'restore-form', 'url' => '', 'method' => 'patch', 'style' => 'display: none;']) !!}
+<!-- PATCH -->
+{!! Form::open(['id' => 'patch-form', 'url' => '', 'method' => 'patch', 'style' => 'display: none;']) !!}
 {!! Form::close() !!}
